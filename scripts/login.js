a25fdc9ae35a8aba966d39da5ecafe5e32a957c2
shrabon10 <omarfaruksrabon@gmail.com>
2026-03-07 15:10:45 +0600

    Succesfully Workable LogIn page

--- scripts/login.js
+++ scripts/login.js
@@ -1,1 +1,16 @@
-const btn = document.querySelector("signIn")
+const signInbtn = document.getElementById("signIn")
+
+signInbtn.addEventListener("click", ()=>{
+    const userName = document.getElementById("userNameValue").value.trim().toLowerCase()  
+    const passward = document.getElementById("passWord").value
+    
+
+    if(userName === "admin" && passward === "admin123"){
+        // alert("sucessfully login") 
+        window.location.assign("index.html")
+        return;
+    }else{
+        alert("Worng Passward")
+        return;
+    }
+})
